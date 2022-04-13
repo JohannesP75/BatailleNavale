ccccde8ad3a0622e6528d2970cff4e72c7d10014
Belhadjabdessamed <belhadjani@gmail.com>
2022-04-13 09:17:05 +0200

    Added Communication code

---
+++ Communication/Communication.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class Communication
+{
+
+    UdpClient udpClient = new UdpClient(1000);
+
+
+    public  void SendMessage(string message, string address)
+    {
+       
+        try
+        {
+             udpClient.Connect(address, 1000);
+            // Sends a message to the host to which you have connected.
+            Byte[] sendBytes = Encoding.ASCII.GetBytes(message);
+
+            udpClient.Send(sendBytes, sendBytes.Length);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    public string ReceiveMessage()
+    {
+      
+
+        //IPEndPoint object will allow us to read datagrams sent from any source.
+        IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+        // Blocks until a message returns on this socket from a remote host.
+        Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+        string returnData = Encoding.ASCII.GetString(receiveBytes);
+
+        return returnData;
+    }
+
+ 
+}
--- testAffichage.cs
+++ testAffichage.cs
@@ -1,4 +1,5 @@
 ﻿// Test affichage
+/*
 namespace BatailleNavale;
 
 class Program
@@ -56,3 +57,62 @@ class Cases
     public char état; // (I)ntact (V)ide (C)oulé
 }
 
+// Test affichage
+namespace BatailleNavale;
+
+class Program
+{
+    static void Main()
+    {
+        public LaGrille cetteGrille { get; set; } = new();
+        cetteGrille.Genere();
+        Affichage.grille(cetteGrille);
+    }
+}
+
+class Affichage
+{
+    public Affichage() // pas utile
+    {
+    }
+    public void grille(LaGrille laGrille)
+    {
+        Console.Clear();
+        for (int Line = 1; Line < 9; Line++)
+        {
+            for (int Col = 1; Col < 9; Col++)
+            {
+                Console.Write(${ laGrille.lesCases[Col, Line].état, 2});
+    }
+    Console.WriteLine();
+        }
+
+    }
+}
+class LaGrille
+{
+    public Cases[,]? lesCases { get; set; }
+    public LaGrille
+        {
+         = new Cases[8, 8];
+
+    public void Genere()
+    {
+        for (int Line = 1; Line < 9; Line++)
+        {
+            for (int Col = 1; Col < 9; Col++)
+            {
+                lesCases[Col, Line].état = (char)(32 + Line + Col);
+                lesCases[Col, Line].identifiant = 0;
+            }
+        }
+    }
+}
+
+class Cases
+{
+    public int identifiant; // ref bâteau ou 0 par défaut
+    public char état; // (I)ntact (V)ide (C)oulé
+}
+
+*/
