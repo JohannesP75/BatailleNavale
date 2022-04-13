63ff316c0ff73ea625f008e79a73d324035962cd
al good <algoodain@gmail.com>
2022-04-13 14:52:51 +0200

    MàJ allocation grille

--- testAffichage.cs
+++ testAffichage.cs
@@ -1,51 +1,60 @@
 ﻿// Test affichage
-//using BatailleNavale;
 
-LaGrille CetteGrille = new();
-CetteGrille.Genere();
+LaGrille GrilleJoueur1 = new();
+LaGrille GrilleJoueur2 = new();
+LaGrille.Genere(GrilleJoueur1);
+LaGrille.Genere(GrilleJoueur2);
 Affichage MonEcran = new();
-MonEcran.Grille(CetteGrille);
+MonEcran.Grille(GrilleJoueur1, true);
+MonEcran.Grille(GrilleJoueur2, false);
 
-//namespace BatailleNavale;
 class Affichage
 {
-    public void Grille(LaGrille laGrille)
+    public void Grille(LaGrille laGrille, bool leJoueur)
     {
         if (laGrille == null) return;
-        Console.Clear();
-        for (int Line = 1; Line < 9; Line++)
+        if (leJoueur)
         {
-            for (int Col = 1; Col < 9; Col++)
+            Console.Clear();
+            Console.WriteLine(" Notre grille:");
+        }
+        else Console.WriteLine("\n Adversaire:");
+        for (int Line = 0; Line < 8; Line++)
+        {
+            if (Line == 0) Console.WriteLine("    A B C D E F G H");
+            Console.Write($"{Line + 1,2} ");
+            for (int Col = 0; Col < 8; Col++)
             {
-                char? cetEtat = laGrille.LesCases?[Col, Line].état;
-                Console.Write($"{ cetEtat, 2}");
+                char? cetEtat = laGrille.LesCases?[Col, Line].etat;
+                Console.Write($"{ cetEtat,2}");
             }
             Console.WriteLine();
         }
 
     }
 }
-internal class LaGrille
+class LaGrille
 {
     public Case[,] LesCases { get; set; } = new Case[8, 8]; // static fait partager le même attribut en mémoire
-    public void Genere()
+    public static void Genere(LaGrille cetteGrille)
     {
-        if (LesCases == null) return;
-        for (int Line = 1; Line < 9; Line++)
+        if (cetteGrille.LesCases == null) return;
+        Case cetteCase = new();
+        for (int Line = 0; Line < 8; Line++)
         {
-            for (int Col = 1; Col < 9; Col++)
+            for (int Col = 0; Col < 8; Col++)
             {
-                LesCases[Col, Line].état = 'C'; // (char)(32 + Line + Col);
-                LesCases[Col, Line].identifiant = 0;
+                cetteCase.etat = 'V'; // (char)(32 + Line + Col);
+                cetteCase.identifiant = 0;
+                // Object reference not set to an instance of an object
+                cetteGrille.LesCases[Col, Line] = cetteCase;
             }
         }
     }
 }
 
-internal class Case
+class Case
 {
-    public int identifiant; // ref bâteau ou 0 par défaut
-    public char état; // (I)ntact (V)ide (C)oulé
+    public int identifiant { get; set; } // ref bâteau ou 0 par défaut
+    public char etat { get; set; } // (I)ntact (V)ide (C)oulé
 }
-
-*/
